ed49499ea7bdf0af623da0a7b26a35d361b6dee5
abernasc21 <89811619+abernasc21@users.noreply.github.com>
2021-12-12 17:49:41 -0400

    Update mensaje.py

--- mensaje.py
+++ mensaje.py
@@ -1,9 +1,10 @@
-# Esta parte son de las funciones para leer y mostrar 
-def mostrarMatriz(a,n,m):#esto solo es una forma para mostrar el mensaje
+# Esta parte son de las funciones para leer y mostrar
+def mostrarMatriz(a,n,m):
+#esto solo es una forma para mostrar el mensaje
     for i in range(0,n):
         print(' '.join(map(str,a[i])))#esta parte elimina los [ , ]
 def leermatriz(a,n,m,mensajeAUX):
-#rellenamos el rectangulo con el mensaje que esta sin ceros     
+#rellenamos el rectangulo con el mensaje que esta sin ceros
     c=0
     for i in range(n):#filas
         b=[0]*m
@@ -12,7 +13,7 @@ def leermatriz(a,n,m,mensajeAUX):
             c+=1
         a[i]=b
 #!--------------------------------------
-# Aqui comienza el codigo para poder armar el mensaje 
+# Aqui comienza el codigo para poder armar el mensaje
 mensaje=input()
 mensaje=mensaje.replace('0',' ') #eliminamos los ceros del mensaje
 mensajeAUX=mensaje
@@ -24,11 +25,10 @@ dimensiones=[]
 for i in range(2,mensaje): #aqui sacamos las dimensiones del mensaje
     if mensaje%i==0:
         dimensiones.append(i)
-print(dimensiones) 
+print(dimensiones)
 print("---------------------")
 #!--------------------------------------
 matriz=[0]*dimensiones[1] #preparamos el rectangulo del mensaje
 leermatriz(matriz,dimensiones[1],dimensiones[0],mensajeAUX) # aqui la funcion busca llenar los espacios del rectangulo
 mostrarMatriz(matriz,dimensiones[1],dimensiones[0])
 #!-----------------------------------------------
-
